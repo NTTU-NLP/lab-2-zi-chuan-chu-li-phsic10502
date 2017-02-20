e4ac1d319a10f3355e1717369e640b1d8fa685b2
phsic10502 <moebiustoklein@yahoo.com.tw>
2017-02-21 02:35:44 +0800

    finished

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] argv)
+        {
+            try
+            {
+                StreamReader src = new StreamReader(@"..\..\..\..\Data\Input.txt");
+                while (src.Peek() != -1)
+                {
+                    string result = Regex.Replace(src.ReadLine(), @"([.?!])", "$0\n");
+                    Console.WriteLine(result);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
